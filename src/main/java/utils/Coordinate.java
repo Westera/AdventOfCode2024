54aa5b34110f48f9a21479b59ee54f29c4fcf96f
AWESTER <awester@volvocars.com>
2024-11-21 09:53:04 +0100

    refactor: Replace Coordinate class with a record

--- src/main/java/utils/Coordinate.java
+++ src/main/java/utils/Coordinate.java
@@ -1,31 +1,9 @@
 package utils;
 
-import java.util.Objects;
-
-public class Coordinate {
-    public final int x;
-    public final int y;
-
-    public Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+public record Coordinate(int x, int y) {
 
     public Coordinate move(int dx, int dy) {
         return new Coordinate(this.x + dx, this.y + dy);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Coordinate that = (Coordinate) obj;
-        return x == that.x && y == that.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
     }
 
     @Override
